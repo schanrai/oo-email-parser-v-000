81a1c247118690d7ff07e352979872f0201f97ba
Sushi Chanrai <sushi@pathworks.co>
2018-07-15 23:42:16 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,9 +4,8 @@
 # or whitespace (' ').
 #`EmailParser` class that gets initialized with a string of emails.
 class EmailParser
-  #Use an `attr_accessor` to set and get the list of emails
+
   attr_accessor :addresses
-  #list of all email addresses
 
   def initialize(emails)
     @addresses = emails
